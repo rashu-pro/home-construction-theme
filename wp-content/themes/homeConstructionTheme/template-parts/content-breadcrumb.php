f5035f1a76e0b420a1e11e5c561bb98a76f990e9
rashu-pro <rashu.pro@gmail.com>
2025-05-26 23:09:23 +0600

    breadcrumb style changed

--- wp-content/themes/homeConstructionTheme/template-parts/content-breadcrumb.php
+++ wp-content/themes/homeConstructionTheme/template-parts/content-breadcrumb.php
@@ -4,6 +4,10 @@
  */
 
 $status = get_field('status_page_breadcrumb');
+$title = get_the_title();
+$words = explode(' ', $title);
+$last_word = array_pop($words);
+$first_part = implode(' ', $words);
 ?>
 
 <!-- Breadcrumb -->
@@ -13,7 +17,8 @@ $status = get_field('status_page_breadcrumb');
         <div class="container">
             <div class="col-xs-12 text-left">
                 <div class="home-builder-page-title">
-                    <h1><?php echo get_the_title() ?></h1>
+                    <h1><?php echo $first_part ?></h1>
+                    <h1 class="titlecolor"><?php echo $last_word ?></h1>
                     <img src="<?php echo get_template_directory_uri() ?>/assets/imgs/linew.png" alt="">
                     <?php custom_breadcrumbs(); ?>
                 </div>
