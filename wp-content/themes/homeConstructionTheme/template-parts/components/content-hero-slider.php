dce19b72312612483969383f2c6e6c6431d336e3
rashu-pro <rashu.pro@gmail.com>
2025-07-25 23:31:09 +0600

    owl carousel has been added in hero slider; video in slider has been added

--- wp-content/themes/homeConstructionTheme/template-parts/components/content-hero-slider.php
+++ wp-content/themes/homeConstructionTheme/template-parts/components/content-hero-slider.php
@@ -31,56 +31,59 @@ $sliders = get_posts($args);
 
 <?php if (!empty($sliders)): ?>
     <!-- Main Carousel -->
-    <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel">
-        <div class="bottom-twt">
-            <div class="container pos-relative">
-                <div class="col-xs-12 text-center">
-                    <div class="news-letter-top text-center" style="position:relative; display: inline-block;"><a
-                                class="scrollup" href="<?php echo site_url() ?>/contact-us"> CONTACT US
-                            <div class="clearfix"></div>
-                            <i class="fa fa-angle-down" aria-hidden="true"></i> </a>
-                        <div class="clearfix"></div>
-                    </div>
-                </div>
-                <div class="clearfix"></div>
-            </div>
-        </div>
-        <div class="scroll-dwn"><a class="page-scroll dwn" href="#about"> <img
-                        src="<?php echo get_template_directory_uri() ?>/assets/imgs/scroll-dwn.png" class="arrow bounce"
-                        alt=""/></a></div>
-        <div class="carousel-indicators">
-            <?php $i = 0; ?>
-            <?php foreach ($sliders as $slider): ?>
-                <?php $active_class = $i === 0 ? 'active' : '' ?>
-                <?php $aria_current = $i === 0 ? 'aria-current="true"' : '' ?>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="<?php echo $i ?>"
-                        class="<?php echo $active_class ?>"
-                    <?php echo $aria_current ?> ></button>
-                <?php $i++; ?>
-            <?php endforeach; ?>
-            <?php wp_reset_postdata() ?>
-        </div>
-        <div class="carousel-inner">
-            <?php $i = 0; ?>
-            <?php foreach ($sliders as $slider): ?>
-                <?php
-                $heading = get_field('slider_heading', $slider->ID);
-                $subheading = get_field('slider_subheading', $slider->ID);
-                $link = get_field('link', $slider->ID);
-                $link_name = get_field('link_name', $slider->ID);
-                $featured_img_url = get_the_post_thumbnail_url($slider->ID, 'full');
-                $video = get_field('slider_video', $slider->ID);
+    <style>
+        .hero-slider .carousel-indicators{
+            bottom: 15%;
+        }
+        .hero-slider-item {
+            height:84vh;
+            min-height:300px;
+            background: no-repeat center center scroll;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+        }
+        .owl-carousel-hero-slider .owl-nav{
+            display: none;
+        }
+        .owl-carousel .owl-dots.disabled, .owl-carousel .owl-nav.disabled{
+            display: none!important;
+        }
+        .owl-carousel.owl-carousel-hero-slider .owl-dots{
+            position: absolute;
+            left: 50%;
+            transform: translateY(-50%);
+            bottom: 2%;
+        }
+        .carousel-caption h1{
+            margin-bottom: 50px;
+        }
+    </style>
 
-                if (empty($link_name)) $link_name = 'Learn More';
-                ?>
-                <?php $active_class = $i === 0 ? 'active' : '' ?>
-                <div class="carousel-item <?php echo $active_class ?>"
-                     style="background-image: url('<?php echo $featured_img_url ?>')">
-                    <div class="pattern"></div>
-                    <video controls autoplay muted loop width="100%">
-                        <source src="<?php echo $video['url'] ?>" type="video/mp4" />
+    <div id="owl-carousel-hero-slider" class="owl-carousel owl-theme owl-carousel-hero-slider">
+        <?php $i = 0; ?>
+        <?php foreach ($sliders as $slider): ?>
+            <?php
+            $heading = get_field('slider_heading', $slider->ID);
+            $subheading = get_field('slider_subheading', $slider->ID);
+            $link = get_field('link', $slider->ID);
+            $link_name = get_field('link_name', $slider->ID);
+            $featured_img_url = get_the_post_thumbnail_url($slider->ID, 'full');
+            $video = get_field('slider_video', $slider->ID);
+
+            if (empty($link_name)) $link_name = 'Learn More';
+            ?>
+            <div class="item hero-slider-item"
+                 style="<?php if (empty($video)) echo "background-image: url('$featured_img_url')" ?>">
+                <div class="pattern"></div>
+                <?php if ($video && $video['url']): ?>
+                    <video controls muted width="100%">
+                        <source src="<?php echo $video['url'] ?>" type="video/<?php echo $video['subtype'] ?>"/>
                         Your browser does not support HTML5 video.
                     </video>
+                <?php endif; ?>
+                <?php if ($heading || $subheading || $link): ?>
                     <div class="carousel-caption">
                         <?php if ($subheading): ?>
                             <h2><?php echo $subheading ?></h2>
@@ -92,10 +95,11 @@ $sliders = get_posts($args);
                             <a href="<?php echo $link ?>" class="btn-creative"><?php echo $link_name ?></a>
                         <?php endif; ?>
                     </div>
-                </div>
-                <?php $i++; ?>
-            <?php endforeach; ?>
-        </div>
+                <?php endif; ?>
+
+            </div>
+            <?php $i++; ?>
+        <?php endforeach; ?>
     </div>
 
     <?php
@@ -107,5 +111,68 @@ $sliders = get_posts($args);
         null,
         true
     );
+
+    add_action('wp_footer', 'hhc_slider_video_control_script', 100);
+    function hhc_slider_video_control_script() {
+        ?>
+        <script>
+            let heroSlider = $('#owl-carousel-hero-slider');
+            const autoPlayTimeout = 5000;
+            heroSlider.owlCarousel({
+                dots: true,
+                loop: true,
+                nav: false,
+                margin: 15,
+                autoplay: true,
+                autoplayTimeout: autoPlayTimeout,
+                responsiveClass: true,
+                items: 1,
+                onInitialized: function(event) {
+                    let currentSlide = $(event.target).find('.owl-item').eq(event.item.index);
+                    let video = currentSlide.find('video');
+                    if(!video.length){
+                        heroSlider.trigger('play.owl.autoplay', [autoPlayTimeout]);
+                        return;
+                    }
+
+                    // Pause autoplay
+                    heroSlider.trigger('stop.owl.autoplay');
+
+                    // Play the video from beginning
+                    video[0].currentTime = 0;
+                    video[0].play();
+
+                    // Resume autoplay after video ends
+                    video.off('ended').on('ended', function () {
+                        heroSlider.trigger('play.owl.autoplay', [autoPlayTimeout]);
+                        heroSlider.trigger('next.owl.carousel');
+                    });
+                }
+            })
+
+            heroSlider.on('changed.owl.carousel', function(event) {
+                let currentSlide = $(event.target).find('.owl-item').eq(event.item.index);
+                let video = currentSlide.find('video');
+                if(!video.length){
+                    heroSlider.trigger('play.owl.autoplay', [autoPlayTimeout]);
+                    return;
+                }
+
+                // Pause autoplay
+                heroSlider.trigger('stop.owl.autoplay');
+
+                // Play the video from beginning
+                video[0].currentTime = 0;
+                video[0].play();
+
+                // Resume autoplay after video ends
+                video.off('ended').on('ended', function () {
+                    heroSlider.trigger('play.owl.autoplay', [autoPlayTimeout]);
+                    heroSlider.trigger('next.owl.carousel');
+                });
+            });
+        </script>
+        <?php
+    }
 
 endif;
