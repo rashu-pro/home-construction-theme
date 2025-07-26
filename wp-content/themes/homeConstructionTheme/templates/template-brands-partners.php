ce254f4cdd22447b8413cd1b1c6db110e0cd0006
rashu-pro <rashu.pro@gmail.com>
2025-07-26 13:39:05 +0600

    brands and partners template created

---
+++ wp-content/themes/homeConstructionTheme/templates/template-brands-partners.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Brands & Partners
+ * Description: Template for brands & Partners listing page
+ */
+
+get_header();
+?>
+    <div class="inner-page">
+        <?php get_template_part('template-parts/components/content', 'breadcrumb') ?>
+        <?php get_template_part('template-parts/content', 'page-brands-partners') ?>
+    </div>
+<?php
+get_footer();
