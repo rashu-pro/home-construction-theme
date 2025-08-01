5c94fde0b3bfa875c92e5da9cf5a57837134acf3
rashu-pro <rashu.pro@gmail.com>
2025-08-01 23:31:41 +0600

    wp_is_mobile() condition added for top header

--- wp-content/themes/homeConstructionTheme/header.php
+++ wp-content/themes/homeConstructionTheme/header.php
@@ -119,57 +119,59 @@
                         </div>
                     </div>
 
-                    <div class="col-sm-9">
-                        <div class="">
-                            <div class="contact-top">
-                                <?php
-                                $address = Redux::get_option('hhc_redux_options', 'hhc_contact_details_address');
-                                $phone = Redux::get_option('hhc_redux_options', 'hhc_contact_details_phone');
-                                $email = Redux::get_option('hhc_redux_options', 'hhc_contact_details_email');
-                                ?>
-                                <div class="row">
-                                    <div class="col-sm-4">
-                                        <div class="row">
-                                            <div class="col-xs-2">
+                    <?php if(!wp_is_mobile()): ?>
+                        <div class="col-sm-9">
+                            <div class="">
+                                <div class="contact-top">
+                                    <?php
+                                    $address = Redux::get_option('hhc_redux_options', 'hhc_contact_details_address');
+                                    $phone = Redux::get_option('hhc_redux_options', 'hhc_contact_details_phone');
+                                    $email = Redux::get_option('hhc_redux_options', 'hhc_contact_details_email');
+                                    ?>
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <div class="row">
+                                                <div class="col-xs-2">
                                                 <span class="head-icon">
                                                     <i class="flaticon-email"></i>
                                                 </span>
-                                            </div>
-                                            <div class="col-xs-10">
-                                                <p class="info-head">Email</p>
-                                                <p><a href="mailto:<?= $email ?>"> <?= $email ?></a> </p>
+                                                </div>
+                                                <div class="col-xs-10">
+                                                    <p class="info-head">Email</p>
+                                                    <p><a href="mailto:<?= $email ?>"> <?= $email ?></a> </p>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
 
-                                    <div class="col-sm-4">
-                                        <div class="col-xs-2">
+                                        <div class="col-sm-4">
+                                            <div class="col-xs-2">
                                             <span class="head-icon">
                                                     <i class="flaticon-technology"></i>
                                                 </span>
+                                            </div>
+                                            <div class="col-xs-10">
+                                                <p class="info-head">Phone</p>
+                                                <p><a href="tel:<?= $phone ?>"> <?= $phone ?></a> </p>
+                                            </div>
                                         </div>
-                                        <div class="col-xs-10">
-                                            <p class="info-head">Phone</p>
-                                            <p><a href="tel:<?= $phone ?>"> <?= $phone ?></a> </p>
-                                        </div>
-                                    </div>
 
-                                    <div class="col-sm-4">
-                                        <div class="col-xs-2">
+                                        <div class="col-sm-4">
+                                            <div class="col-xs-2">
                                             <span class="head-icon">
                                                     <i class="flaticon-placeholder"></i>
                                                 </span>
-                                        </div>
-                                        <div class="col-xs-10">
-                                            <p class="info-head">Address</p>
-                                            <p><?= $address ?> </p>
+                                            </div>
+                                            <div class="col-xs-10">
+                                                <p class="info-head">Address</p>
+                                                <p><?= $address ?> </p>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
 
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    <?php endif; ?>
                 </div>
             </div>
             <div class="clearfix"></div>
