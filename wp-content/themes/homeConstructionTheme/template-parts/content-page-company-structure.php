c254f1cad21e790b92c99bde6ebc2716d5732274
rashu-pro <rashu.pro@gmail.com>
2025-08-01 14:27:13 +0600

    company structure tree designed using chatgpt

---
+++ wp-content/themes/homeConstructionTheme/template-parts/content-page-company-structure.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Page for company structure
+ */
+?>
+
+<div class="inner-page content-section">
+    <?php get_template_part('template-parts/components/content', 'company-structure') ?>
+</div>
